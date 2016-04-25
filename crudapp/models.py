3732b5705a78943f0a45b816cecb68c620431858
shanegibney <artgibney@gmail.com>
2016-04-25 16:16:00 +0100

    Update models.py

--- crudapp/models.py
+++ crudapp/models.py
@@ -11,13 +11,3 @@ class Members(models.Model):
     def __str__(self):
         return ' '. join([ self.first_name, self.last_name, ])
 
-        # def __str__(self):
-        #     return self.last_name
-        #
-        # def __str__(self):
-        #     return self.description
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
