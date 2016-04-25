4a4a3a81f59b30e67544527ded7c2afb0fa4734b
sanegibley <shanegibney@gmail.com>
2016-04-25 16:14:57 +0100

    added a model

---
+++ crudapp/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Members',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=200)),
+                ('last_name', models.CharField(max_length=200)),
+                ('description', models.CharField(max_length=600)),
+            ],
+        ),
+    ]
--- crudapp/models.py
+++ crudapp/models.py
@@ -1,5 +1,23 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+class Members(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=600)
+    # pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return ' '. join([ self.first_name, self.last_name, ])
+
+        # def __str__(self):
+        #     return self.last_name
+        #
+        # def __str__(self):
+        #     return self.description
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
