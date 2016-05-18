e7d0262c1cdb3a59da88f6fd209ed2b04ee3fd08
shanegibney <shanegibney@gmail.com>
2016-05-18 17:17:02 +0100

    Added profile for users and statistics to forum, thread and profiles

--- crudProject/settings.py
+++ crudProject/settings.py
@@ -35,8 +35,8 @@ DEFAULT_FROM_EMAIL = 'shanegibney@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shanegibney@gmail.com'
-EMAIL_HOST_PASSWORD = 'd7sx82na'
+EMAIL_HOST_USER = '******************'
+EMAIL_HOST_PASSWORD = '**********'
 EMAIL_USE_TLS = True
 
 # Application definition
